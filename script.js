30c1c38733980b412f7050394860244edf6e3e0b
jay32den <siskjayden09@gmail.com>
2024-03-17 00:06:42 -0500

    minor changes

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
+var trick = document.getElementById('flatGround');
+var ledge = document.getElementById('ledgeTrick');
+var rail = document.getElementById('railTrick');
+var outTrick = document.getElementById('trickOut');
+
 function gentext() {
-    var flattrick= ['kickflip','heelflip','treflip','ollie',];
-    var trick = document.getElementById('flatGround');
-    trick.innerHTML = flattrick[Math.floor(Math.random() * flattrick.length)];
+    var flattrick = ['kickflip', 'heelflip', 'treflip', 'ollie'];
+    showText(trick, flattrick);
 }
+
 function genledge() {
-    var ledgetrick = ['50-50','5.0', 'Smith', 'Back-Smith', 'Front-Tail','Back-Tail',];
-    var ledge =document.getElementById ('ledgeTrick');
-    ledge.innerHTML = ledgetrick [Math.floor(Math.random() * ledgetrick.length)];
+    var ledgetrick = ['50-50', '5.0', 'Smith', 'Back-Smith', 'Front-Tail', 'Back-Tail'];
+    showText(ledge, ledgetrick);
+}
+
+function genrail() {
+    var railTrick = ['50-50', '5.0', 'Smith', 'Back-Smith', 'Front-Tail', 'Back-Tail', 'Front-Feeble'];
+    showText(rail, railTrick);
+}
+
+function genTrickOut() {
+    var trickOut = ['kickflip-fakie', 'kickflip-regular', 'hardflip', 'heelflip-fakie', 'heelflip-regular', 'treflip', 'ollie'];
+    showText(outTrick, trickOut);
+}
+
+function showText(element, tricks) {
+    // Create a div for the centered text
+    var centeredText = document.createElement('div');
+    centeredText.id = 'centeredText';
+    centeredText.textContent = tricks[Math.floor(Math.random() * tricks.length)];
+
+    // Append the div to the body
+    document.body.appendChild(centeredText);
+
+    // Remove the div after 1 seconds (adjust as needed)
+    setTimeout(function () {
+        centeredText.remove();
+    }, 1000);
 }
